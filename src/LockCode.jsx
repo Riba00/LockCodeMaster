1e2db14b43e35ceb2ce8618c6f929b49ce090cd4
Riba00 <jordiriba@iesebre.com>
2023-09-06 17:21:47 +0200

    Acabat ara si

--- src/LockCode.jsx
+++ src/LockCode.jsx
@@ -3,11 +3,11 @@ import { PassGenerator } from "./components/PassGenerator";
 
 export const LockCode = () => {
   return (
-    <div className="min-h-screen relative isolate overflow-hidden bg-gray-900 px-6 py-16 shadow-2xl">
+    <div className="min-h-screen relative isolate overflow-hidden bg-gray-900 px-6 py-6 shadow-2xl">
       <h1 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
         LockCode Master
       </h1>
-      <p className="mx-auto mt-2 max-w-xl text-center text-lg leading-8 text-gray-300">
+      <p className="mx-auto mt-6 max-w-xl text-center text-lg text-gray-300">
         Welcome to <b>LockCode Master</b>, where we transform ordinary passwords
         into extraordinary safeguards for your online world. <br />We offer two
         services: <b>Password Generator</b> and <b>Password Tester</b>.{" "}
--- src/components/PassChecker.jsx
+++ src/components/PassChecker.jsx
@@ -52,7 +52,7 @@ export const PassChecker = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-          placeholder="Enter your password"
+          placeholder="Enter your password to test the strenght"
         />
       </div>
       {password && (
